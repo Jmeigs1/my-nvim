c9328b5a5161be8ad31cdea6490e7c2dc1b55316
Jake Meigs <jakemeigs@gmail.com>
2024-01-28 02:02:35 -0500

    Fix floating terminal

--- lua/toggleterm-config.lua
+++ lua/toggleterm-config.lua
@@ -41,20 +41,13 @@ end
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-function _LAZYGIT_TOGGLE()
-  lazygit:toggle()
-end
 
 local node = Terminal:new({ cmd = "node", hidden = true })
-
 function _NODE_TOGGLE()
   node:toggle()
 end
 
 local python = Terminal:new({ cmd = "python3", hidden = true })
-
 function _PYTHON_TOGGLE()
   python:toggle()
 end
@@ -64,12 +57,7 @@ require("which-key").register({
     name = "Terminal",
     n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" }, -- Node Terminal
     p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" }, -- Python Terminal
-    f = {
-      function()
-        Terminal:new({ count = vim.v.count, direction = "float" })
-      end,
-      "Float",
-    }, -- Floating Terminal
+    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" }, -- Horizontal Terminal,
 
     h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" }, -- Horizontal Terminal,
     v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" }, -- Vertical Terminal

13bfb4f3df8c9cc5e02b651a08352acfe6fe154d
Jacob Meigs <1722522+Jmeigs1@users.noreply.github.com>
2024-12-09 17:44:22 -0500

    Add ocaml extra + merlin lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,32 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-  },
+  opts = function(_, opts)
+    return vim.tbl_deep_extend("force", opts, {
+      inlay_hints = { enabled = false },
+      servers = {
+        ocamllsp = {
+          cmd = { "ocamllsp", "--fallback-read-dot-merlin" },
+          filetypes = {
+            "ocaml",
+            "ocaml.menhir",
+            "ocaml.interface",
+            "ocaml.ocamllex",
+            "reason",
+            "dune",
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "*.opam",
+              "esy.json",
+              "package.json",
+              ".git",
+              "dune-project",
+              "dune-workspace",
+              "*.ml"
+            )(fname)
+          end,
+        },
+      },
+    })
+  end,
 }
